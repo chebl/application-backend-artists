3bd276a138450698b038cb1b10d0d49dcbcb263a
chebl <chebl.mahmoud@gmail.com>
2019-01-03 13:28:13 +0100

    Import the dataset by using fixtures

--- src/DataFixtures/ArtistsFixtures.php
+++ src/DataFixtures/ArtistsFixtures.php
@@ -14,11 +14,18 @@ class ArtistsFixtures extends Fixture
     {
         $content=file_get_contents('https://gist.github.com/fightbulc/9b8df4e22c2da963cf8ccf96422437fe/raw/8d61579f7d0b32ba128ffbf1481e03f4f6722e17/artist-albums.json');              
         $content = json_decode($content, 1);
-	    $token=TokenGenerator::generate();
-	    foreach($content as $artists) {
+	    $tokensArtists=array();
+	    $tokensAlbums=array();
+		foreach($content as $artists) {
           /**Save Artist data **/
+		  
 		  $token=TokenGenerator::generate();
-	      $name=$artists['name'];
+	      while(in_array($token,$tokensArtists)){
+		   $token=TokenGenerator::generate();
+	      }
+		  $tokensArtists[]=$token;
+		  
+		  $name=$artists['name'];
 		  $artist = new Artists(); 
 		  $artist->setToken($token);
 		  $artist->setName($name);
@@ -27,8 +34,15 @@ class ArtistsFixtures extends Fixture
 		  /** Save related Albums**/
 		  foreach($artists['albums'] as $albums){
 		   $album=new Albums();
+		   
 		   $token=TokenGenerator::generate();
-	       $title=$albums['title'];
+	      
+	      while(in_array($token,$tokensAlbums)){
+		   $token=TokenGenerator::generate();
+	      }
+		  $tokensAlbums[]=$token;
+		  
+		   $title=$albums['title'];
 		   $description=$albums['description'];
 	       $cover=$albums['cover'];
 		   $album->setToken($token);
